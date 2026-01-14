fe662a795e36f435c502bcb36dcd4e3c508e65df
Robin Lane <rubeculabee@gmail.com>
2026-01-13 17:22:55 -0800

    implemented creation of blog posts dynamically from text files

--- JS/blog.js
+++ JS/blog.js
@@ -4,16 +4,49 @@ fr = new FileReader()
 
 function onLoad()
 {
-	getPosts()
+	getPosts().then(post => makePages(post))
 }
 
 async function getPosts()
 {
+	var post = {
+		date: '',
+		title: '',
+		text: ''
+	}
+
 	response = await fetch("../Metadata/blog-posts/Lorem Ipsum.txt")
 	file = await response.blob()
-	await fr.readAsText(file)
 
-	fr.onload = () => {
-		console.log(fr.result)
+	post.date = file.lastModified
+	post.title = file.name
+
+	fr.readAsText(file)
+	return new Promise((resolve) => {
+		fr.onload = () => {
+			post.text = fr.result
+			resolve(post)
+		}
+	});
+}
+
+function makePages(post)
+{
+	page = document.createElement('div')
+	page.setAttribute('class', 'page')
+
+	content = {
+		date: document.createElement('i'),
+		title: document.createElement('b'),
+		text: document.createElement('p')
 	}
+
+	for(let part in content)
+	{
+		console.log(`${content[part]} ${part}: ${post[part]}`)
+		content[part].innerText = post[part]
+		page.appendChild(content[part])
+	}
+
+	document.body.appendChild(page)
 }
